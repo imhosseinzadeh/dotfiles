8871e19ce5f9920827ec10797037835b32edcb28
imhosseinzadeh <imhosseinzadeh@gmail.com>
2025-02-15 14:58:17 +0330

    add neovim configurations

---
+++ nvim/init.lua
@@ -0,0 +1,21 @@
+-- ===================================================================
+-- =============              NEOVIM CONFIG              =============
+-- ===================================================================
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = true
+
+-- Load core settings
+require('core.options')
+require('core.keymaps')
+require('core.autocmds')
+
+-- Load plugin configurations
+require('plugins.init')
+
