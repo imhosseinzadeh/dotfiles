9e5eb4aa9cd5f7461e1fb7c5780c47d4566f95d3
imhosseinzadeh <imhosseinzadeh@gmail.com>
2025-02-24 15:06:33 +0330

    add cpu temperature and major improvments

--- sway/status.sh
+++ sway/status.sh
@@ -1,72 +1,70 @@
 #!/bin/bash
 
-# The Sway configuration file in ~/.config/sway/config calls this script.
-# You should see changes to the status bar after saving this script.
-# If not, do "killall swaybar" and $mod+Shift+c to reload the configuration.
-
 # Emojis and characters for the status bar
-UPTIME_EMOJI="⌛"
 CPU_EMOJI="🚀"
+CPU_TEMP_EMOJI="🌡️"
 MEMORY_EMOJI="💾"
-VOLUME_EMOJI="🔊"
 BLUETOOTH_EMOJI="🔵"
-NETWORK_EMOJI="🌐"
-IP_EMOJI="📡"
+WIFI_EMOJI="📶"
 BATTERY_EMOJI="🔋"
-PLUGGED_BATTERY_EMOJI="🔌"
+PLUGGED_EMOJI="🔌"
 DATE_EMOJI="📅"
 TIME_EMOJI="🕒"
 SEPARATOR="|"
 
-# Get system uptime in a human-readable format
-uptime_formatted=$(uptime -p | awk '{print $2"H:"$4"M"}')
-
-# Get battery status (if available)
-battery_status="N/A"
-if [[ -e /sys/class/power_supply/BAT1/status ]]; then
-    battery_status=$(cat /sys/class/power_supply/BAT1/status)
-fi
-
-# Get battery status and percentage (if available)
-battery_status="N/A"
-battery_percentage="N/A"
-if [[ -e /sys/class/power_supply/BAT1/status ]] && [[ -e /sys/class/power_supply/BAT1/capacity ]]; then
-    battery_status=$(cat /sys/class/power_supply/BAT1/status)
-    battery_percentage=$(cat /sys/class/power_supply/BAT1/capacity)
-fi
-
-# Check if laptop is connected to electricity and determine the battery emoji to use
-battery_emoji=$BATTERY_EMOJI
-if [[ "$battery_status" == "Charging" ]]; then
-    battery_emoji=$PLUGGED_BATTERY_EMOJI
-fi
-
 # Get load average
 load_average=$(uptime | awk -F 'load average: ' '{print $2}' | cut -d ',' -f1)
+
+# CPU Temperature
+cpu_temp=$(sensors | awk '/Package id 0:/ {print $4}' | tr -d '+')
 
 # Get memory usage
 mem_usage=$(free -h | awk '/Mem:/ {print $3 "/" $2}')
 
-# Get network status and IP address
-network_status=$(ip link show | awk '/state UP/ {print $2}' | tr -d ':')
-ip_address=$(hostname -I | awk '{print $1}')
-
 # Get audio volume using wpctl
 volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print $2*100"%"}')
+
+# Set the volume emoji based on volume
+if [[ "$volume" == "0%" ]]; then
+    volume_emoji="🔇"
+    volume="Muted"
+else
+    volume_emoji="🔊"
+    volume="$volume"
+fi
 
 # Get connected Bluetooth device
 bluetooth_device=$(bluetoothctl info | grep "Name" | awk -F ': ' '{print $2}')
 
+# Wi-Fi Name and Strength
+wifi_info=$(nmcli -t -f active,ssid,signal dev wifi | grep '^yes' | cut -d':' -f2,3)
+wifi_name=$(echo $wifi_info | cut -d':' -f1)
+wifi_strength=$(echo $wifi_info | cut -d':' -f2)
+
+# Power Information
+battery_info=$(upower -i $(upower -e | grep 'BAT'))
+battery_status=$(echo "$battery_info" | grep 'state' | awk '{print $2}')
+battery_percentage=$(echo "$battery_info" | grep 'percentage' | awk '{print $2}')
+if [[ "$battery_status" == "discharging" ]]; then
+    battery_emoji=$BATTERY_EMOJI
+else
+    battery_emoji=$PLUGGED_EMOJI
+fi
+
 # Get the current date and time in a formatted string
-date_formatted=$(date "+%a %Y-%m-%d")
+date_formatted=$(date +"%d %b")
 time_formatted=$(date "+%H:%M")
 
 # Assemble the status bar output
-status_bar="$UPTIME_EMOJI $uptime_formatted $SEPARATOR \
-$CPU_EMOJI $load_average $SEPARATOR \
+status_bar="$CPU_EMOJI $load_average $SEPARATOR \
+$CPU_TEMP_EMOJI $cpu_temp $SEPARATOR \
 $MEMORY_EMOJI $mem_usage $SEPARATOR \
-$battery_emoji $battery_status (${battery_percentage}%) $SEPARATOR \
-$VOLUME_EMOJI $volume"
+$volume_emoji $volume $SEPARATOR"
+
+# Add wifi information to the status bar if connected
+if [[ -n "$wifi_name" ]]; then
+    status_bar="$status_bar $WIFI_EMOJI $wifi_name ($wifi_strength%) $SEPARATOR"
+fi
 
 # Add Bluetooth device to the status bar if connected
 if [[ -n "$bluetooth_device" ]]; then
@@ -74,7 +72,8 @@ if [[ -n "$bluetooth_device" ]]; then
 fi
 
 # Continue assembling the status bar output
-status_bar="$status_bar $SEPARATOR $NETWORK_EMOJI $network_status $IP_EMOJI $ip_address $SEPARATOR \
+status_bar="$status_bar \
+$battery_emoji $battery_status (${battery_percentage}) $SEPARATOR \
 $DATE_EMOJI $date_formatted $SEPARATOR $TIME_EMOJI $time_formatted"
 
 # Output the status bar
