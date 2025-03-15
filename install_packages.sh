93a6869c25578b300b20f83019f616647f75ea62
imhosseinzadeh <imhosseinzadeh@gmail.com>
2025-03-16 00:20:41 +0330

    add some packages and reorder packages

--- install_packages.sh
+++ install_packages.sh
@@ -48,41 +48,64 @@ fi
 
 # List of packages to install
 PACKAGES=(
+    # System Essentials and CLI Utilities
     pacman-contrib
     bash-completion
-    tree
     man
+    tree
     wl-clipboard
+
+    # Editors and Terminal Emulators
     vim
-    luarocks
     neovim
-    rtkit
-    wmenu
     alacritty
+
+    # Miscellaneous CLI Tools
+    wmenu
+    rtkit
+
+    # File and Media Management
+    unrar
     imv
-    noto-fonts
-    noto-fonts-emoji
-    ttf-nerd-fonts-symbols
     thunar
     thunar-volman
     thunar-archive-plugin
     gvfs
+    gvfs-mtp
+    mtpfs
     xarchiver
     dosfstools
     exfatprogs
+
+    # Fonts
+    noto-fonts
+    noto-fonts-emoji
+    ttf-nerd-fonts-symbols
+
+    # Development Tools
     git
     gcc
     go
     openjdk21-src
+    npm
+    luarocks
+
+    # Containerization and Databases
     docker
     docker-compose
     postgresql
+
+    # Web and Communication
     firefox
+    telegram-desktop
     keepassxc
+
+    # Multimedia Players and Audio
     mpv
     vlc
     cmus
-    telegram-desktop
+
+    # TeX / Document Processing
     texlive-basic
     texlive-xetex
     texlive-langarabic
